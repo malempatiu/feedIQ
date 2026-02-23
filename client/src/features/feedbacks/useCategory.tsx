72b85031efe810c3a0f8756402d10f58279f0e06
ugesh malempati <malempatiu@gmail.com>
2026-02-23 18:07:29 +0100

    poll for category

---
+++ client/src/features/feedbacks/useCategory.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { client } from "@api/Api";
+
+type Feedback = {
+  id: number;
+  title: string;
+  detail: string;
+  category: string;
+  priority?: string;
+  createdAt: string;
+  updatedAt?: string;
+  votes: number;
+  commentsCount: number;
+};
+
+const useCategory = (id: number, enabled: boolean) => {
+  const { data, error } = useQuery({
+    queryKey: ["feed", { id }],
+    queryFn: () => client.get<Feedback>(`feeds/${id}`),
+    enabled,
+    refetchInterval: (query) => {
+      console.log(query)
+      const hasPending = !query.state.data?.data?.category;
+      return hasPending ? 3000 : false;
+    },
+  });
+
+  return {
+    feedback: data?.data,
+    showLoading: enabled && !data?.data?.category,
+    errorMessage: error?.message,
+  };
+};
+
+export { useCategory };
