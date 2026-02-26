4bc32bd28220d962a2560462e76562388f740d08
ugesh malempati <malempatiu@gmail.com>
2026-02-26 16:38:13 +0100

    hide inngest docs from documentation

--- server/src/main.py
+++ server/src/main.py
@@ -10,7 +10,16 @@ origins = [
     "http://localhost:5173",
 ]
 
-app = FastAPI(title='feedIQ server')
+version = 'v1'
+
+app = FastAPI(
+    title='feedIQ server',
+    description='A REST API for feedIQ (A product feedback app)',
+    version=version,
+    docs_url=f"/api/{version}/docs",
+    redoc_url=f"/api/{version}/redoc",
+)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -19,12 +28,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get('/')
 async def read_root():
     return {
         'message': 'Hello from feedIQ server :)'
     }
 
-app.include_router(router=feeds_router, prefix='/api/v1/feeds', tags=['feedbacks'])
-app.include_router(router=user_router, prefix='/api/v1/auth', tags=['authentication'])
+app.include_router(router=feeds_router,
+                   prefix=f"/api/{version}/feeds", tags=['feedbacks'])
+app.include_router(router=user_router,
+                   prefix=f"/api/{version}/auth", tags=['authentication'])
 fast_api.serve(app, inngest_client, [categorize_feedback_background])
+
+# Hide Inngest API from Swagger documentation
+original_openapi = app.openapi
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = original_openapi()
+
+    # Filter out Inngest routes from paths
+    if openapi_schema and "paths" in openapi_schema:
+        inngest_paths = [
+            path for path in openapi_schema["paths"] if "/api/inngest" in path]
+        for path in inngest_paths:
+            del openapi_schema["paths"][path]
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
