05e9807646dd424b778ddb729a7b75347f28fb6b
ugesh malempati <malempatiu@gmail.com>
2026-03-10 20:25:21 +0100

    implement basic Kafka Producer to publish events

--- server/src/background_tasks/tasks/categorize_feedback_bg.py
+++ server/src/background_tasks/tasks/categorize_feedback_bg.py
@@ -3,6 +3,22 @@ from src.background_tasks.client import inngest_client
 from src.feeds.dtos import FeedbackCreateDTO
 from pydantic import BaseModel
 from src.feeds.factory import get_feeds_service
+from src.message_broker.producer import kafka_producer
+
+
+async def produce(id: int, category: str, title: str, detail: str) -> None:
+    await kafka_producer.produce(
+        topic="feedbacks",
+        value={
+            "event": "feedback_categorized",
+            "id": id,
+            "category": category,
+            "title": title,
+            "detail": detail,
+        },
+        key=str(id),
+    )
+
 
 class FeedbackCategorizeEvent(BaseModel):
     id: int
@@ -22,6 +38,7 @@ async def categorize_feedback_background(ctx: Context):
 
         async with get_feeds_service() as (feeds_service, session):
             dto = FeedbackCreateDTO(title=event.title, detail=event.detail)
+
             async def categorize():
                 return await feeds_service.categorize_feedback(
                     id=event.id,
@@ -29,6 +46,12 @@ async def categorize_feedback_background(ctx: Context):
                 )
             result = await ctx.step.run("categorize", categorize)
             ctx.logger.info(f"Successfully categorized feedback {event.id}")
+
+            async def produce_event():
+                return await produce(id=event.id, category=result if result else 'Uncategorized', title=event.title, detail=event.detail)
+
+            await ctx.step.run("produce-kafka-event", produce_event)
+            ctx.logger.info(f"Kafka event produced for feedback id={event.id}")
             return {"success": True, "feedback_id": event.id, "result": result}
     except ValueError as e:
         ctx.logger.error(f"Invalid event data: {e}")
